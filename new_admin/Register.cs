81d30e0946d77f17aff879eba288f114ab7702b7
adarsh1694 <adarsh@longeo.in>
2020-03-12 11:06:34 +0530

    Add project files.

---
+++ new_admin/Register.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace new_admin
+{
+    public partial class Register : Form
+    {
+        IFirebaseConfig Config = new FirebaseConfig
+        {
+            AuthSecret = FireSharpConfig.AuthSecret,
+            BasePath = FireSharpConfig.BasePath
+        };
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void lab_reg_click
+    }
+}
